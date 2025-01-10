902a92201384a56b47e65058a607b5ac7b7eb12d
Mason Gray <24graym@gmail.com>
2025-01-10 17:21:41 -0600

    pieces fall, can move side to side and detect each other

--- classes.py
+++ classes.py
@@ -10,7 +10,11 @@ class Game:
     red = pygame.image.load("images/red-tile.png")
     purple = pygame.image.load("images/purple-tile.png")
 
-    idToImage = {1:lightBlue, 2:darkBlue)}
+    idToImage = {1:lightBlue, 2:darkBlue, 3:orange, 4:yellow, 5:green, 6:purple, 7:red}
+
+    activePiece = None
+    activeCords = [] #cords of active piece
+    activeCenter = () #center of active piece
 
     def __init__(self, rows, cols):
         self.board = [[0 for i in range(cols)] for j in range(rows)]
@@ -20,8 +24,45 @@ class Game:
             print(i)
 
     def spawn(self, piece):
-        for x, y in piece.cords:
+        for x, y in piece.sCords:
             self.board[y][x] = piece.id
+
+    def movePiece(self):
+        oldCords = self.activeCords
+        newCords = []
+        for x, y in oldCords:
+            newCords.append([x, y+1])
+            if y+1 > len(self.board) - 1 or (self.board[y+1][x] != 0 and [x, y+1] not in oldCords): #make sure piece can fall
+                return False
+
+        for x, y in oldCords:
+            self.board[y][x] = 0
+
+        for x, y in newCords:
+            self.board[y][x] = self.activePiece.id
+
+        self.activeCords = newCords
+        self.activeCenter[1] += 1
+
+        return True
+
+    def shift(self, shiftAmt): #-1 goes left, 1 goes right
+        oldCords = self.activeCords
+        newCords = []
+        for x, y in oldCords:
+            newCords.append([x+shiftAmt, y])
+
+            if x + shiftAmt < 0 or x + shiftAmt > len(self.board[0]) - 1 or (self.board[y][x+shiftAmt] != 0 and [x+shiftAmt, y] not in oldCords): #finds out if shift is invalid
+                return
+
+        for x, y in oldCords:
+            self.board[y][x] = 0
+
+        for x, y in newCords:
+            self.board[y][x] = self.activePiece.id
+
+        self.activeCords = newCords
+        self.activeCenter[0] += shiftAmt
 
 '''PIECE OUTLINE
                   []              []      [][]
@@ -36,8 +77,62 @@ Spawn positions relative to top left corner of board, in terms of tiles. Top lef
 class I:
 
     def __init__(self):
-        spawnPos = [(3, 1), (4, 1), (5, 1), (6, 1)]
-        spawnCenter = (4.5, 1.5)
-        self.cords = spawnPos
-        self.center = spawnCenter
+        spawnPos = [[3, 1], [4, 1], [5, 1], [6, 1]]
+        spawnCenter = [4.5, 1.5]
+        self.sCords = spawnPos
+        self.sCenter = spawnCenter
         self.id = 1
+
+class J:
+
+    def __init__(self):
+        spawnPos = [[3, 0], [3, 1], [4, 1], [5, 1]]
+        spawnCenter = [4, 1]
+        self.sCords = spawnPos
+        self.sCenter = spawnCenter
+        self.id = 2
+
+class L:
+
+    def __init__(self):
+        spawnPos = [[3, 1], [4, 1], [5, 1], [5, 0]]
+        spawnCenter = [4, 1]
+        self.sCords = spawnPos
+        self.sCenter = spawnCenter
+        self.id = 3
+
+class O:
+
+    def __init__(self):
+        spawnPos = [[4, 0], [5, 0], [5, 1], [4, 1]]
+        spawnCenter = [4.5, 0.5]
+        self.sCords = spawnPos
+        self.sCenter = spawnCenter
+        self.id = 4
+
+class S:
+
+    def __init__(self):
+        spawnPos = [[3, 1], [4, 1], [4, 0], [5, 0]]
+        spawnCenter = [4, 1]
+        self.sCords = spawnPos
+        self.sCenter = spawnCenter
+        self.id = 5
+
+class T:
+
+    def __init__(self):
+        spawnPos = [[3, 1], [4, 1], [4, 0], [5, 1]]
+        spawnCenter = [4, 1]
+        self.sCords = spawnPos
+        self.sCenter = spawnCenter
+        self.id = 6
+
+class Z:
+
+    def __init__(self):
+        spawnPos = [[3, 0], [4, 0], [4, 1], [5, 1]]
+        spawnCenter = [4, 1]
+        self.sCords = spawnPos
+        self.sCenter = spawnCenter
+        self.id = 7
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
-from classes import Game, I
+import random
+from classes import Game, I, J, L, O, S, T, Z
 
 pygame.init()
 clock = pygame.time.Clock()
@@ -12,13 +13,16 @@ COLS = 10
 BOARD_W, BOARD_H = CELL_SIZE*COLS, CELL_SIZE*ROWS
 TPADDING = (HEIGHT - CELL_SIZE*ROWS) // 2  # the empty space at the top of the game
 
+pieces = [I, J, L, O, S, T, Z]
+turnTime = 60 # how long for each tick, in frames
+deltaTime = 1 # subtracted from timer
+
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
 game = Game(ROWS, COLS)
 
-game.print()
-
 def drawBoard(screen):
+    screen.fill("black")
     startX = (WIDTH - CELL_SIZE*COLS) // 2
     #draw the grid
     for row in range(ROWS + 1):  # horizontal
@@ -29,21 +33,40 @@ def drawBoard(screen):
 
     for i in range(ROWS):
         for j in range(COLS):
+            if game.board[i][j] != 0:
+                screen.blit(game.idToImage[game.board[i][j]], (startX + j*CELL_SIZE, TPADDING + i*CELL_SIZE)) #takes a number from the board and draws the correct tile at the correct position
 
 
-i = I()
-game.spawn(i)
-print()
-game.print()
-
+getNewActive = True #do we need a new active piece
 
 run = True
 while run:
     clock.tick(FPS)
+    screen.fill("black")
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                game.shift(1)
+            if event.key == pygame.K_LEFT:
+                game.shift(-1)
+
+    #starts a new active piece
+    if getNewActive:
+        game.activePiece = random.choice(pieces)()
+        game.spawn(game.activePiece)
+        game.activeCords = game.activePiece.sCords
+        game.activeCenter = game.activePiece.sCenter
+        getNewActive = False
+
+    # Timer for falling piece, triggers new piece when piece reaches bottom
+    turnTime -= deltaTime
+    if turnTime <= 0:
+        if not game.movePiece():
+            getNewActive = True
+        turnTime = 60
 
     drawBoard(screen)
 
