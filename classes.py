dc44154acd3e527edf7a40dfe446aded6f15614e
Mason Gray <24graym@gmail.com>
2025-01-11 18:55:34 -0600

    ghost piece added, instant drop added

--- classes.py
+++ classes.py
@@ -1,5 +1,6 @@
 import pygame
-
+pygame.init()
+import copy
 
 class Game:
     lightBlue = pygame.image.load("images/light-blue-tile.png")
@@ -10,14 +11,28 @@ class Game:
     red = pygame.image.load("images/red-tile.png")
     purple = pygame.image.load("images/purple-tile.png")
 
-    idToImage = {1:lightBlue, 2:darkBlue, 3:orange, 4:yellow, 5:green, 6:purple, 7:red}
-
-    activePiece = None
-    activeCords = [] #cords of active piece
-    activeCenter = () #center of active piece
+    transparency = 80
 
     def __init__(self, rows, cols):
         self.board = [[0 for _ in range(cols)] for _ in range(rows)]
+        self.boardCopy = copy.deepcopy(self.board)
+        self.activePiece = None
+        self.activeCords = []  # cords of active piece
+        self.activeCenter = ()  # center of active piece
+        self.ghostCords = []
+        self.idToImage = {1: self.lightBlue, 2: self.darkBlue, 3: self.orange, 4: self.yellow, 5: self.green, 6: self.purple, 7: self.red,
+                        11: self.lightBlue.convert_alpha(),
+                        22: self.darkBlue.convert_alpha(),
+                        33: self.orange.convert_alpha(),
+                        44: self.yellow.convert_alpha(),
+                        55: self.green.convert_alpha(),
+                        66: self.purple.convert_alpha(),
+                        77: self.red.convert_alpha()}
+        for i in range(1, 8):
+            key = int(str(i)*2)
+            self.idToImage[key].set_alpha(self.transparency)
+            if key == 66:
+                self.idToImage[key].set_alpha(self.transparency + 50) #makes the purple a bit brighter
 
     def print(self):
         for i in self.board:
@@ -32,7 +47,8 @@ class Game:
         newCords = []
         for x, y in oldCords:
             newCords.append([int(x), int(y+1)])
-            if y+1 > len(self.board) - 1 or (self.board[y+1][x] != 0 and [x, y+1] not in oldCords): #make sure piece can fall
+
+            if y+1 > len(self.board) - 1 or (self.board[y+1][x] != 0 and self.board[y+1][x] <= 10 and [x, y+1] not in oldCords): #make sure piece can fall
                 return False
 
         for x, y in oldCords:
@@ -51,7 +67,7 @@ class Game:
         newCords = []
         for x, y in oldCords:
             newCords.append([x+shiftAmt, y])
-            if x + shiftAmt < 0 or x + shiftAmt > len(self.board[0]) - 1 or (self.board[y][x+shiftAmt] != 0 and [x+shiftAmt, y] not in oldCords): #finds out if shift is invalid
+            if x + shiftAmt < 0 or x + shiftAmt > len(self.board[0]) - 1 or (self.board[y][x+shiftAmt] != 0  and self.board[y][x+shiftAmt] <= 10 and [x+shiftAmt, y] not in oldCords): #finds out if shift is invalid
                 return
 
         for x, y in oldCords:
@@ -83,7 +99,7 @@ class Game:
                 testx = int(x+xshift)
                 testy = int(y+yshift)
                 if (testx < 0 or testx > len(self.board[0]) - 1 or testy < 0 or testy > len(self.board) - 1 or
-                        (self.board[testy][testx] != 0 and [testx, testy] not in oldCords)): # checks if the rotation/wallkick is valid
+                        (self.board[testy][testx] != 0  and self.board[testy][testx] <= 10 and [testx, testy] not in oldCords)): # checks if the rotation/wallkick is valid
                     valid = False
                     break
             if valid:
@@ -101,9 +117,25 @@ class Game:
         for x, y in rotatedCords:
             self.board[y][x] = self.activePiece.id #had to add int() due to weird floating point errors
 
-        print(wallKickID)
         piece.rotationState = desiredRotationState
         self.activeCords = rotatedCords
+
+    def updateGhost(self):
+        for x, y in self.ghostCords:
+            self.boardCopy[y][x] = 0
+
+        downwardShift = 1 #how far down to draw ghost piece
+        valid = True
+        while valid:
+            for x, y in self.activeCords:
+                if y + downwardShift > len(self.board) - 1 or (self.board[y+downwardShift][x] != 0 and [x, y+downwardShift] not in self.activeCords):
+                    valid = False
+                    break
+            self.ghostCords = [[x,y+downwardShift-1] for x,y in self.activeCords]
+            downwardShift += 1
+
+        for x, y in self.ghostCords:
+            self.boardCopy[y][x] = int(str(self.activePiece.id)*2)
 
 '''PIECE OUTLINE
                   []              []      [][]
@@ -149,6 +181,7 @@ class I:
     def __init__(self):
         spawnPos = [[3, 1], [4, 1], [5, 1], [6, 1]]
         spawnCenter = [4.5, 1.5]
+        self.nextShift = (24*7+30+12, 44) #represents a shift in pixels to go from spawnPos to the next area
         self.sCords = spawnPos
         self.sCenter = spawnCenter
         self.id = 1
@@ -160,6 +193,7 @@ class J:
     def __init__(self):
         spawnPos = [[3, 0], [3, 1], [4, 1], [5, 1]]
         spawnCenter = [4, 1]
+        self.nextShift = (24 * 7 + 30 + 24, 56)
         self.sCords = spawnPos
         self.sCenter = spawnCenter
         self.id = 2
@@ -171,6 +205,7 @@ class L:
     def __init__(self):
         spawnPos = [[3, 1], [4, 1], [5, 1], [5, 0]]
         spawnCenter = [4, 1]
+        self.nextShift = (24 * 7 + 30 + 24, 56)
         self.sCords = spawnPos
         self.sCenter = spawnCenter
         self.id = 3
@@ -182,6 +217,7 @@ class O:
     def __init__(self):
         spawnPos = [[4, 0], [5, 0], [5, 1], [4, 1]]
         spawnCenter = [4.5, 0.5]
+        self.nextShift = (24 * 6 + 30 + 36, 56)
         self.sCords = spawnPos
         self.sCenter = spawnCenter
         self.id = 4
@@ -193,6 +229,7 @@ class S:
     def __init__(self):
         spawnPos = [[3, 1], [4, 1], [4, 0], [5, 0]]
         spawnCenter = [4, 1]
+        self.nextShift = (24 * 7 + 30 + 24, 56)
         self.sCords = spawnPos
         self.sCenter = spawnCenter
         self.id = 5
@@ -204,6 +241,7 @@ class T:
     def __init__(self):
         spawnPos = [[3, 1], [4, 1], [4, 0], [5, 1]]
         spawnCenter = [4, 1]
+        self.nextShift = (24 * 7 + 30 + 24, 56)
         self.sCords = spawnPos
         self.sCenter = spawnCenter
         self.id = 6
@@ -215,6 +253,7 @@ class Z:
     def __init__(self):
         spawnPos = [[3, 0], [4, 0], [4, 1], [5, 1]]
         spawnCenter = [4, 1]
+        self.nextShift = (24 * 7 + 30 + 24, 56)
         self.sCords = spawnPos
         self.sCenter = spawnCenter
         self.id = 7
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import random
 from classes import Game, I, J, L, O, S, T, Z
 
 pygame.init()
+pygame.font.init()
 clock = pygame.time.Clock()
 FPS = 60
 
@@ -12,6 +13,8 @@ ROWS = 20
 COLS = 10
 BOARD_W, BOARD_H = CELL_SIZE*COLS, CELL_SIZE*ROWS
 TPADDING = (HEIGHT - CELL_SIZE*ROWS) // 2  # the empty space at the top of the game
+font = pygame.font.Font("fonts/simpleblock.ttf", 24)
+nextText = font.render('Next', False, "black")
 
 pieces = [I, J, L, O, S, T, Z]
 turnTime = 30 # how long for each tick, in frames
@@ -21,6 +24,7 @@ deltaTime = 1 # subtracted from timer
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
 game = Game(ROWS, COLS)
+
 
 def drawBoard(screen):
     screen.fill("black")
@@ -34,11 +38,22 @@ def drawBoard(screen):
 
     for i in range(ROWS):
         for j in range(COLS):
-            if game.board[i][j] != 0:
+            if game.board[i][j] == 0 and game.boardCopy[i][j] != 0:
+                screen.blit(game.idToImage[game.boardCopy[i][j]], (startX + j*CELL_SIZE, TPADDING + i*CELL_SIZE)) #takes a number from the board and draws the correct tile at the correct position
+            elif game.board[i][j] != 0:
                 screen.blit(game.idToImage[game.board[i][j]], (startX + j*CELL_SIZE, TPADDING + i*CELL_SIZE)) #takes a number from the board and draws the correct tile at the correct position
 
+def drawNextBox(screen, pieceNext):
+    pygame.draw.line(screen, "white", (450, TPADDING + 10), (570, TPADDING + 10), 21) #top
+    pygame.draw.line(screen, "white", (450, TPADDING), (450, TPADDING + 140)) #left
+    pygame.draw.line(screen, "white", (570, TPADDING), (570, TPADDING + 140)) #right
+    pygame.draw.line(screen, "white", (450, TPADDING + 140), (570, TPADDING + 140)) #bottom
+    screen.blit(nextText, (452, TPADDING))
+    for x, y in pieceNext.sCords:
+        screen.blit(game.idToImage[pieceNext.id], (x*CELL_SIZE+180+pieceNext.nextShift[0], y*CELL_SIZE+TPADDING+pieceNext.nextShift[1]))
 
 getNewActive = True #do we need a new active piece
+pieceNext = random.choice(pieces)()
 
 run = True
 while run:
@@ -51,28 +66,45 @@ while run:
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_RIGHT:
                 game.shift(1)
-            if event.key == pygame.K_LEFT:
+            elif event.key == pygame.K_LEFT:
                 game.shift(-1)
-            if event.key == pygame.K_UP:
+            elif event.key == pygame.K_UP:
                 game.rotate(game.activePiece, 1)
-            if event.key == pygame.K_z:
+            elif event.key == pygame.K_z:
                 game.rotate(game.activePiece, -1)
+            elif event.key == pygame.K_SPACE:
+                while game.movePiece():
+                    pass
+                game.activePiece = pieceNext
+                pieceNext = random.choice(pieces)()
+                game.spawn(game.activePiece)
+                game.activeCords = game.activePiece.sCords
+                game.activeCenter = game.activePiece.sCenter
+                getNewActive = False
+            game.updateGhost()
 
     #starts a new active piece
     if getNewActive:
-        game.activePiece = random.choice(pieces)()
+        game.activePiece = pieceNext
+        pieceNext = random.choice(pieces)()
         game.spawn(game.activePiece)
         game.activeCords = game.activePiece.sCords
         game.activeCenter = game.activePiece.sCenter
         getNewActive = False
+
 
     # Timer for falling piece, triggers new piece when piece reaches bottom
     currentTime -= deltaTime
     if currentTime <= 0:
         if not game.movePiece():
             getNewActive = True
+
+        game.updateGhost()
         currentTime = turnTime
 
+
     drawBoard(screen)
+    drawNextBox(screen, pieceNext)
+
 
     pygame.display.flip()
